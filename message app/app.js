0fddcc27531a57181432f57fe749b5855e633ff1
Shubh0622 <shubhambachhav8@gmail.com>
2024-04-11 19:36:22 +0530

    basic message app

---
+++ message app/app.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const fs =require('fs');
+const server = http.createServer((req,res)=>{
+    const url=req.url;
+    const method =req.method;
+    if(url==='/'){
+        const message = fs.readFileSync('F:/SharpenerProjects/BackendQuestions/message app/message.txt','utf8');
+        res.write('<html>');
+        res.write('<head><title>Message App</title></head>');
+        res.write('<body>');
+        res.write(`<p>${message}<\p>`);
+        res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
+        res.write('</html>');
+        return res.end();
+    }
+    if(url==='/message' && method==='POST'){
+        const body=[];
+        req.on('data',(chunk)=>{
+            body.push(chunk);
+        });
+        req.on('end',()=>{
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt',message);
+        });
+        res.writeHead(302,  {Location: "/"});
+        return res.end();
+    }
+    // res.setHeader('Content-Type','text/html');
+    // res.write('<html>');
+    // res.write('<head><title>First App</title></head>');
+    // res.write('<body><h1>Welcome home</h1></body>');
+    // res.write('</html>');
+    // res.end();
+});
+server.listen(4000);
