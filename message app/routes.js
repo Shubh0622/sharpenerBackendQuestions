ddb6bb1781295f2ed7869f4e15c35404232d6590
Shubh0622 <shubhambachhav8@gmail.com>
2024-04-12 14:25:28 +0530

    code cleanup by adding routes.js and exporting it.

---
+++ message app/routes.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+
+const requestHandler = ((req,res)=>{
+    const url=req.url;
+    const method =req.method;
+    if(url==='/'){
+        let message;
+        if(fs.existsSync('F:/SharpenerProjects/BackendQuestions/message app/message.txt')){
+            message = fs.readFileSync('F:/SharpenerProjects/BackendQuestions/message app/message.txt','utf8');
+        }
+        else{
+            message="";
+        }
+        res.write('<html>');
+        res.write('<head><title>Message App</title></head>');
+        res.write('<body>');
+        res.write(`<p>${message}<\p>`);
+        res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
+        res.write('</html>');
+        return res.end();
+    }
+    if(url==='/message' && method==='POST'){
+        const body=[];
+        req.on('data',(chunk)=>{
+            body.push(chunk);
+        });
+        req.on('end',()=>{
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt',message);
+        });
+        res.writeHead(302,  {Location: "/"});
+        return res.end();
+    }
+    // res.setHeader('Content-Type','text/html');
+    // res.write('<html>');
+    // res.write('<head><title>First App</title></head>');
+    // res.write('<body><h1>Welcome home</h1></body>');
+    // res.write('</html>');
+    // res.end();
+})
+
+module.exports = requestHandler;
