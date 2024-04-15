4c888108d88e9072157a54bbfb1cda2d110dbfa1
Shubh0622 <shubhambachhav8@gmail.com>
2024-04-15 14:28:38 +0530

    handling routes

--- Express/app.js
+++ Express/app.js
@@ -1,14 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log('Inside middleware');
-    next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><label for="title">product</label><input type="text" name="title"><label for="quantity">quantity</label><input type="number" name="quantity"><button type="submit">Submit</button></form>');
 })
-app.use((req,res,next)=>{
-    console.log('inside another middleware');
-    res.send('<h1>Hello from Express!</h1>');
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    console.log(req.body.title);
+    console.log(req.body.quantity);
+    res.redirect('/');
 })
 
+app.use('/',(req,res,next)=>{
+    res.send("<h1>Welcome to Express!</h1>");
+})
 app.listen(4000);
